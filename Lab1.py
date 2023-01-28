681753251351916148c59dd743bf3c509db59f4f
EC2 Default User <ec2-user@ip-172-31-49-172.ec2.internal>
2023-01-28 02:16:44 +0000

    my lab2.py file.

--- Lab1.py
+++ Lab1.py
@@ -1,1 +1,25 @@
-print('hello world')
+print(type('hello world'))# string need marks (string covers allllll)
+print(type(123))# integer does not need marks 
+print(type(123.123))# floating 
+
+print("hello")
+print('hello')
+print("'EEEEEEEEEEEEEEEEEe'")
+
+print("Hello World".upper())
+print("Hello World".lower())
+
+print(" hello world ".strip())# REMOVES SPACE
+
+print("hello world".split("o")) # split by o
+print('hello '+"world")
+print(1/2)# can do calculations wwww0000w0w0
+my_num = 123# you can always change the value of the varibles
+print(my_num)
+
+my_num = "tom"
+print(my_num.upper())
+
+my_id = "123"
+print(my_id)
+my_id = your_id = "123"
---
+++ lab2.py
@@ -0,0 +1,36 @@
+'''
+lab 2
+'''
+
+#6.1 
+my_name = 'Tom'
+print(my_name.upper())
+
+#6.2
+my_id = 123
+print(my_id)
+
+#6.3
+#123= my_id
+#print(my_id)
+my_id = your_id = 123
+
+my_id_str = str("123")
+print(my_id_str)
+
+#print(my_name + my_id)
+
+
+print(my_name + my_id_str)
+#You CAN add two strings
+
+print(my_name*3)
+
+print("Hello, world. This is my first python assighnment.".split(". "))
+
+message ='Toms id is 123'
+print(message)
+
+git add --all 
+git commit -m "my lab2.py file."   
+git push
