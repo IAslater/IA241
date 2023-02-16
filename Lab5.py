1ddce08732eb17760469cbeea774ebab02ccd63f
EC2 Default User <ec2-user@ip-172-31-49-172.ec2.internal>
2023-02-16 22:03:58 +0000

    some notes here.

---
+++ Lab5.py
@@ -0,0 +1,27 @@
+#3.1
+alien_color = "yellow"
+if alien_color == "green" :
+    print("you got 5 points")
+    
+#3.2
+if alien_color == "green" : 
+    print("you got 5 points")
+else :
+    print ("you got 10 points")
+    
+    #3.3
+favorite_fruits = ['a', "g", "b"]
+
+if "p" in favorite_fruits :
+    print ('you really like p!')
+    
+    #3.4
+age = 98
+if age<10 :
+    print("this user is a kid")
+elif age <20 :
+    print ("this user is a teen")
+else:
+    print ("this user is an adult")
+    if age >65:   
+     print ("this user is an elder")
